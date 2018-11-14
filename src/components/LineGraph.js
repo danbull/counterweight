023f68b25e449ce169e28a34ead52a9e37a358b6
margotr <github@margotrutgers.nl>
2018-11-14 17:13:44 +0000

    add danger zones to graph

--- src/components/LineGraph.js
+++ src/components/LineGraph.js
@@ -22,7 +22,9 @@ class LineGraph extends React.Component {
                 type: 'spline',
                 inverted: true,
             },
+            tooltip: { enabled: false },
             series: [{
+                color: '#000000',
                 data: [85.3, 84.2, 84, 83.1, 83.9].map(x => x - targetWeight)
             }],
             xAxis: {
@@ -43,7 +45,29 @@ class LineGraph extends React.Component {
 
     setChartInstance(chart) {
         this.chart = chart;
-    }
+        var x1a = chart.yAxis[0].toPixels(1);
+        var x1b = chart.yAxis[0].toPixels(-2);
+        var x2 = chart.xAxis[0].toPixels(0);
+
+        var width =  chart.yAxis[0].toPixels(2) - chart.yAxis[0].toPixels(1)
+        var height = chart.xAxis[0].toPixels(4) - chart.xAxis[0].toPixels(0)
+
+        //Add 'danger zone'
+        this.chart.renderer.rect(x1a, x2, width, height).attr({
+            'stroke-width': 2,
+            fill: 'rgb(255, 0, 0, 0.5)',
+            zIndex: 1
+        })
+        .add();
+
+        //Add 'danger zone'
+        this.chart.renderer.rect(x1b, x2, width, height).attr({
+            'stroke-width': 2,
+            fill: 'rgb(255, 255, 0, 0.5)',
+            zIndex: 1
+        })
+        .add();
+      }
 
     render() {
         return (
