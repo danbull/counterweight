530493cbe81e8fcd8fede92b809d380602070a83
Margot Rutgers <margot@kayanazimov.local>
2018-11-15 10:07:11 +0000

    Add tooltip for today

--- src/components/LineGraph.js
+++ src/components/LineGraph.js
@@ -2,6 +2,7 @@ import React from 'react'
 import Highcharts from 'highcharts'
 import HighchartsReact from 'highcharts-react-official'
 
+require("highcharts/modules/annotations")(Highcharts);
 window['Highcharts'] = Highcharts;
 
 
@@ -12,6 +13,7 @@ class LineGraph extends React.Component {
         this.setChartInstance = this.setChartInstance.bind(this);
 
         var targetWeight = 83.5;
+        var data = [85.3, 84.2, 84, 83.1, 83.9].map(x => x - targetWeight)
 
         var options_vertical = {
             legend: {
@@ -25,7 +27,7 @@ class LineGraph extends React.Component {
             tooltip: { enabled: false },
             series: [{
                 color: '#000000',
-                data: [85.3, 84.2, 84, 83.1, 83.9].map(x => x - targetWeight)
+                data: data
             }],
             xAxis: {
                 visible: false,
@@ -37,10 +39,18 @@ class LineGraph extends React.Component {
                 tickPositions: [-2, -1, 0, 1, 2],
                 endOnTick: true
             },
+            annotations: [{
+                labels: [{
+                    point: {
+                        x: 1, y: data[1], 
+                        xAxis: 0,
+                        yAxis: 0,
+                    },
+                    text: "Today",
+                }],
+            }],
         }
-
         this.state = { options: options_vertical };
-
     }
 
     setChartInstance(chart) {
@@ -49,7 +59,7 @@ class LineGraph extends React.Component {
         var x1b = chart.yAxis[0].toPixels(-2);
         var x2 = chart.xAxis[0].toPixels(0);
 
-        var width =  chart.yAxis[0].toPixels(2) - chart.yAxis[0].toPixels(1)
+        var width = chart.yAxis[0].toPixels(2) - chart.yAxis[0].toPixels(1)
         var height = chart.xAxis[0].toPixels(4) - chart.xAxis[0].toPixels(0)
 
         //Add 'danger zone'
@@ -58,7 +68,7 @@ class LineGraph extends React.Component {
             fill: 'rgb(255, 0, 0, 0.5)',
             zIndex: 1
         })
-        .add();
+            .add();
 
         //Add 'danger zone'
         this.chart.renderer.rect(x1b, x2, width, height).attr({
@@ -66,8 +76,8 @@ class LineGraph extends React.Component {
             fill: 'rgb(255, 255, 0, 0.5)',
             zIndex: 1
         })
-        .add();
-      }
+            .add();
+    }
 
     render() {
         return (
