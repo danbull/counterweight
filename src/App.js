fff64671a6c781e8d25d9df0f40be3cd5c900e45
Margot Rutgers <margot@kayanazimov.local>
2018-11-29 11:00:36 +0000

    Added response card to weight change. Somehow can't get it to show on state change.

--- src/App.js
+++ src/App.js
@@ -5,7 +5,8 @@ import Footer from "./components/Footer";
 import base from "./utils/base";
 import RecordWeight from "./components/RecordWeight";
 import "./App.css";
-import ContentBanner from "./components/ContentBanner";
+import ContentBanner from "./components/WeightResponseCard";
+import WeightResponseCard from "./components/WeightResponseCard";
 
 class Weight extends Component {
   constructor() {
@@ -18,6 +19,7 @@ class Weight extends Component {
       chartData: null,
       weighins: null,
       showWeightInput: false,
+      showResponseCard: false,
       weightChangeState: 0,
     };
 
@@ -38,44 +40,54 @@ class Weight extends Component {
   }
 
   updateWeight(weight) {
-    console.log("New weight:")
+    console.log("New weight: ")
     console.log(weight);
-    
+
+    // In reality it should be something like this:
+    // delta-time = time-of-last-weight-measurement - time-of-current-weight-measurement (get in hours, between 0 and 24)
+    // delta-weight = new weight - last-weight  
+    // allowed-weight-fluctuation = delta-time / 24 *  2.5 KG (= maximum normal weight fluctuation) 
+    // if (abs(delta-weight) < allowed-weight-fluctuation) {
+    //  'nothing to see here' 
+    // else {
+    // positive or negative weight loss  
+    // }
+
     let delta = weight - this.state.currentWeight;
     let weightChange = 0;
 
-    if (delta > 0.2) {
+    if (delta > 0.5) {
       weightChange = 1;
-
-    } else if (delta < -0.2) {
+    } else if (delta < -0.5) {
       weightChange = -1;
-
     }
-    
+
     this.setState({
-      weightChangeState: weightChange
+      weightChangeState: weightChange,
+      showResponseCard: true,
     })
 
+    console.log("Updated weight, should show response card: " + this.state.showResponseCard)
   }
 
   componentDidMount() {
     base
-    .fetch("users/cYzlSenFtBVM5GYsLZsIf1vSsKr2", {
-      context: this,
-      asArray: false
-    })
-    .then(data => {
-      console.log("data", data);
-      this.setState({
-        targetWeight:  data.tagetWeight,
-        currentWeight: data.weighins[data.weighins.length -1].weight
+      .fetch("users/cYzlSenFtBVM5GYsLZsIf1vSsKr2", {
+        context: this,
+        asArray: false
       })
-    });
+      .then(data => {
+        console.log("data", data);
+        this.setState({
+          targetWeight: data.tagetWeight,
+          currentWeight: data.weighins[data.weighins.length - 1].weight
+        })
+      });
   }
 
   render() {
     return (
-      <div className="App"> 
+      <div className="App">
         <Header />
         <div className="progress-status">
           <span>Week 6</span>
@@ -83,15 +95,16 @@ class Weight extends Component {
         <div className="home">
           <div className="hero">
             <Link to={"/weight-summary"}><span></span>Current weight <span className="unit">{this.state.currentWeight}</span></Link>
+            <WeightResponseCard weightChangeState={this.state.weightChangeState} show={this.state.showResponseCard}/>
           </div>
         </div>
         <div>
-        { this.state.showWeightInput === true ? (
+          {this.state.showWeightInput === true ? (
             <RecordWeight currentWeight={this.state.currentWeight} closeRecordOverlay={this.closeRecordOverlay} updateWeight={this.updateWeight.bind(this)} />
-          ) : 
-          null }
+          ) :
+            null}
         </div>
-        <Footer handleRecordWeightClick={this.handleRecordWeightClick} showWeightInput={this.state.showWeightInput}  />
+        <Footer handleRecordWeightClick={this.handleRecordWeightClick} showWeightInput={this.state.showWeightInput} />
       </div>
     );
   }
---
+++ src/components/WeightResponseCard.js
@@ -0,0 +1,75 @@
+import React, { Component } from "react";
+import '../WeightResponseCard.css'
+
+class WeightResponseCard extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            image: 'https://sloanreview.mit.edu/content/uploads/2017/04/DL-Mukherjee-Digital-Leadership-Neutrality-Neutral-Culture-1200-300x300.jpg',
+            title: 'Default',
+            body: 'Some default text',
+            show: this.props.show,
+        };
+        console.log("In the constructor - " + this.state.show)
+    }
+
+    componentWillMount() {
+        this.setAppropriateContent();
+    }
+
+    setAppropriateContent(props){
+
+        let content; 
+
+        if (this.props.weightChangeState === -1 ){
+            content = {
+                image: 'http://www.elmundodelpapelpintado.com/242540-large_default/confeti.jpg',
+                title: 'Good work',
+                body: 'Well done!',
+            }
+        } else if (this.props.weightChangeState === 1) {
+            content = {
+                image: 'http://media.istockphoto.com/vectors/huge-helping-hand-new-business-concept-vector-id530718785?k=6&m=530718785&s=612x612&w=0&h=pArbjjYryXf4eA0eSXBjrkEmWBZgEd3GnFioN0Lp8TU=',
+                title: "Let's get back on track...",
+                body: "Here is some super useful content",
+            }
+        } else if (this.props.weightChangeState === 0){
+            content = {
+                image: 'http://media.istockphoto.com/vectors/huge-helping-hand-new-business-concept-vector-id530718785?k=6&m=530718785&s=612x612&w=0&h=pArbjjYryXf4eA0eSXBjrkEmWBZgEd3GnFioN0Lp8TU=',
+                title: "Nothing changed",
+                body: "Here is some super useful content",
+                show: false
+            }
+        }
+        this.setState((state,props) => (content));
+    }
+
+    close() {
+        this.setState({
+            show: false,
+        })
+    }
+
+    render() {
+
+        const style = this.state.show ? {} : {display: 'none'} ;
+
+        return (
+            <div style={style} className="card-container">
+                <div className="response-card">
+                    <img alt="description" src={this.state.image} className="response-image" />
+                    <div className="card-title">
+                        <h1>{this.state.title}</h1>
+                    </div>
+                    <div className="card-body">
+                        {this.state.body}
+                    </div>
+                    <button className="button close-button" onClick={this.close.bind(this)}>Close</button>
+                </div>
+            </div>
+        );
+    }
+}
+
+export default WeightResponseCard;
